d3d8173d778601ac07e1361f5c7fd28c35806b93
ArunRangrej <arunrangrej1997@gmail.com>
2023-11-01 11:25:51 +0530

    Setup tools to install required libraries

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='sales-data-processor',
+    version='1.0',
+    description='Sales Data Processor for Company XYZ',
+    author='abc',
+    author_email='abc@gmail.com',
+    packages=[],
+    install_requires=[
+        'sqlite3',
+        'pandas',
+    ],
+    test_suite='tests',
+)
